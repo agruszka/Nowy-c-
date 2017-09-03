5683641b855558bcf8c4ba68c61b04230b9657ba
Jacek Gmoch <jacek.gmoch@gmail.com>
2017-09-03 13:21:24 +0200

    Zadanie 1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cześć, jak masz na imię?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Powiedz mi jeszcze ile masz lat?");
+            string wiek;
+            wiek = Console.ReadLine();
+
+            Console.WriteLine("Cześć " + name + ", fajnie, że masz " + wiek + " lat");
+            Console.ReadLine();
+        }
+    }
+}
